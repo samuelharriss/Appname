567df818a8225aca0c08aa9aabd16f3ffbfd392c
samuel harris <samuel.harris1188@gmail.com>
2020-08-14 14:22:52 +1200

    _

--- app.js
+++ app.js
@@ -1,41 +1,60 @@
-var createError = require('http-errors');
+// Add all required modules
 var express = require('express');
+var session = require('express-session');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var fileUpload = require('express-fileupload');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+/** 
+ * Instantiate the Express app and 
+ * assign it to a global variable
+ * psuedo namespaced under AraDT
+ */
+AraDTApp = express();
+AraDTApp.use(session({
+    secret: 'hurrdurr',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}));
 
-var app = express();
+// Assign view directory and EJS template language
+AraDTApp.set('views', path.join(__dirname, 'views'));
+AraDTApp.set('view engine', 'ejs');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+/** 
+ * Add key middleware for:
+ *      File uploads
+ *      Console logging requests
+ *      JSON parsing
+ *      Request string parsing
+ */ 
+AraDTApp.use(fileUpload({createParentPath: true}));
+AraDTApp.use(logger('dev'));
+AraDTApp.use(express.json());
+AraDTApp.use(express.urlencoded({ extended: false }));
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// Assign static files directory
+AraDTApp.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+/**
+ * Add simple MVC framework including:
+ *      Firebase database class
+ *      Image uploader
+ *      Data validator
+ *      Key models
+ *      A router
+ * All are psuedo namespaced under AraDT
+ */
+var Database = require('./classes/Database'); 
+AraDTDatabase = new Database(); 
+var ImageUpload = require('./classes/ImageUpload');
+AraDTImageUpload = new ImageUpload();
+var Validator = require('./classes/Validator');
+AraDTValidator = new Validator();
+var UserModel = require('./models/UserModel');
+AraDTUserModel = new UserModel();
+var Router = require('./classes/Router');
+AraDTRouter = new Router();
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+module.exports = AraDTApp;
